6a5d4af2bb13d98f1fbf4c7c424daa34da234ad1
Abdur Rakib <rakibabdur960@gmail.com>
2020-10-06 15:59:53 +0600

    client site is done

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCu_9LsncqQ0yIe3t-Uy6xS4j5nGXyES4M",
+    authDomain: "volunteer-auth.firebaseapp.com",
+    databaseURL: "https://volunteer-auth.firebaseio.com",
+    projectId: "volunteer-auth",
+    storageBucket: "volunteer-auth.appspot.com",
+    messagingSenderId: "874289410850",
+    appId: "1:874289410850:web:fe37f154c58cefea606b7b"
+  };
+  
+    export default firebaseConfig;
