80dba693c1aa51352f4be58bcd0825a938773518
Abdur Rakib <rakibabdur960@gmail.com>
2020-10-06 17:33:57 +0600

    fav-icon modify

---
+++ build/precache-manifest.d2cadd319c1e9399fdda6f0b7a33caa2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c412a793498fd9c390604a815862925e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14870ae130f6425a70db",
+    "url": "/static/css/main.5ae963ba.chunk.css"
+  },
+  {
+    "revision": "ef33cf5564619b50cbf1",
+    "url": "/static/js/2.6583f3a7.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.6583f3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14870ae130f6425a70db",
+    "url": "/static/js/main.22acee08.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d499438e41fd0bb4b96e9cf30dab7e60",
+    "url": "/static/media/backgroud.d499438e.jpg"
+  },
+  {
+    "revision": "f743201257a980013c200b299ca3b9d9",
+    "url": "/static/media/google.f7432012.png"
+  },
+  {
+    "revision": "b7dd8bbef4a0ef3ceba26c40007aaf82",
+    "url": "/static/media/logo.b7dd8bbe.png"
+  }
+]);
